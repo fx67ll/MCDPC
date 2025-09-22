42fd439407af5b4e9e5623128f92523427de9fe8
“fx67ll;git config --global user.name “fx67ll <2717745014@qq.com>
2025-09-22 10:35:06 +0800

    add lushu

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="fx67ll-box">
-		<div class="fx67ll-title"><span @click="linktoFx67ll">fx67ll's 大数据可视化地图示例</span></div>
+		<div class="fx67ll-title"><span @click="linktoFx67ll">fx67ll's 大数据可视化地图相关示例合集</span></div>
 		<div class="fx67ll-link">
 			<div class="fx67ll-link-item"><router-link tag="span" to="/mapcanvas">CanvasMap Tool Based on
 					GaoDeMap</router-link></div>
@@ -20,12 +20,17 @@
 			<div class="fx67ll-link-item">
 				<span><a target="_blank" href="https://www.yuque.com/docs/share/66ee9f75-5135-445f-a997-b476a5f342bf?# 《fx67llQuickEcharts》">fx67llQuickEcharts</a></span>
 			</div> -->
+			<div class="fx67ll-link-item">
+				<span><a target="_blank" href="https://ai.fx67ll.com/national-day-self-driving-plan">National Day Self Driving Plan Powered By AI</a></span>
+			</div>
 			<div class="fx67ll-link-item"><router-link tag="span" to="/test">Code Mirror</router-link></div>
 			<div class="fx67ll-link-item" v-show="isDev"><router-link tag="span" to="/devtest">Component Test
 					Tool</router-link></div>
 			<div class="fx67ll-link-item"><span class="fx67ll-devTip">持续开发更新中......</span></div>
 		</div>
-		<iframe class="fx67ll-background" src="https://test.fx67ll.com/fx67ll-background-collection/html/borealSky/borealSky.html" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
+		<iframe class="fx67ll-background"
+			src="https://test.fx67ll.com/fx67ll-background-collection/html/borealSky/borealSky.html" width="100%"
+			height="100%" frameborder="0" scrolling="no"></iframe>
 		<div class="fx67ll-footer">
 			Designed & Powered by
 			<a href="https://fx67ll.com" target="_blank">fx67ll</a>
@@ -36,110 +41,110 @@
 </template>
 
 <script>
-	import moment from 'moment';
-	export default {
-		name: 'fx67llIndex',
-		data() {
-			return {
-				year: moment().format('YYYY')
-			};
-		},
-		computed: {
-			// 判断是否是开发环境，组件测试页面仅在开发环境开放
-			isDev() {
-				return process.env.VUE_APP_ENV === 'development';
-			}
-		},
-		methods: {
-			linktoFx67ll() {
-				console.log(
-					'####### #     #  #####  ####### #       #     \n#        #   #  #     # #    #  #       #       \n#         # #   #           #   #       #       \n#####      #    ######     #    #       #       \n#         # #   #     #   #     #       #\n#        #   #  #     #   #     #       #\n#       #     #  #####    #     ####### ####### '
-				);
-				window.open('https://fx67ll.xyz');
-			}
+import moment from 'moment';
+export default {
+	name: 'fx67llIndex',
+	data() {
+		return {
+			year: moment().format('YYYY')
+		};
+	},
+	computed: {
+		// 判断是否是开发环境，组件测试页面仅在开发环境开放
+		isDev() {
+			return process.env.VUE_APP_ENV === 'development';
 		}
-	};
+	},
+	methods: {
+		linktoFx67ll() {
+			console.log(
+				'####### #     #  #####  ####### #       #     \n#        #   #  #     # #    #  #       #       \n#         # #   #           #   #       #       \n#####      #    ######     #    #       #       \n#         # #   #     #   #     #       #\n#        #   #  #     #   #     #       #\n#       #     #  #####    #     ####### ####### '
+			);
+			window.open('https://fx67ll.xyz');
+		}
+	}
+};
 </script>
 
 <style lang="less" scoped="scoped">
-	@textZindex: 2;
-	@textColor: #ffff00;
+@textZindex: 2;
+@textColor: #ffff00;
 
-	.fx67ll-box {
-		width: 100%;
-		height: 100%;
-		.ban-user-select();
+.fx67ll-box {
+	width: 100%;
+	height: 100%;
+	.ban-user-select();
 
-		.fx67ll-title {
-			padding: 36px 20px 24px 20px;
-			text-align: center;
-			position: relative;
-			z-index: @textZindex;
+	.fx67ll-title {
+		padding: 36px 20px 24px 20px;
+		text-align: center;
+		position: relative;
+		z-index: @textZindex;
+		color: @green;
+
+		span {
+			display: inline-block;
+			font-size: 36px;
+		}
+
+		span:hover {
 			color: @green;
+		}
+	}
+
+	@linkMarginTop: 10px;
+	@linkMarginBotton: 20px;
+
+	.fx67ll-link {
+		max-height: calc(~'100% - @{linkMarginTop} - @{linkMarginBotton} - 113px');
+		overflow: auto;
+		padding: 0 20px;
+		margin: @linkMarginTop 0 @linkMarginBotton 0;
+		position: relative;
+		z-index: @textZindex;
+
+		.fx67ll-link-item {
+			width: 100%;
+			margin-bottom: 25px;
+			text-align: center;
+			color: @textColor;
 
 			span {
 				display: inline-block;
-				font-size: 36px;
+				font-size: 24px;
 			}
 
 			span:hover {
 				color: @green;
 			}
-		}
 
-		@linkMarginTop: 10px;
-		@linkMarginBotton: 20px;
-
-		.fx67ll-link {
-			max-height: calc(~'100% - @{linkMarginTop} - @{linkMarginBotton} - 113px');
-			overflow: auto;
-			padding: 0 20px;
-			margin: @linkMarginTop 0 @linkMarginBotton 0;
-			position: relative;
-			z-index: @textZindex;
-
-			.fx67ll-link-item {
-				width: 100%;
-				margin-bottom: 25px;
-				text-align: center;
+			a {
+				text-decoration: none;
 				color: @textColor;
-
-				span {
-					display: inline-block;
-					font-size: 24px;
-				}
-
-				span:hover {
-					color: @green;
-				}
-
-				a {
-					text-decoration: none;
-					color: @textColor;
-				}
-
-				.fx67ll-devTip {
-					// color: #ff5500;
-					color: @red;
-					font-size: 14px;
-				}
 			}
-		}
 
-		.fx67ll-background {
-			position: absolute;
-			top: 0;
-			z-index: @textZindex - 1;
-		}
-
-		.fx67ll-footer {
-			z-index: @textZindex;
-		}
-
-		@media screen and (max-width: 960px) {
-			.fx67ll-footer {
-				font-size: 12px;
+			.fx67ll-devTip {
+				// color: #ff5500;
+				color: @red;
+				font-size: 14px;
 			}
 		}
 	}
+
+	.fx67ll-background {
+		position: absolute;
+		top: 0;
+		z-index: @textZindex - 1;
+	}
+
+	.fx67ll-footer {
+		z-index: @textZindex;
+	}
+
+	@media screen and (max-width: 960px) {
+		.fx67ll-footer {
+			font-size: 12px;
+		}
+	}
+}
 </style>
