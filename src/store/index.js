6a3a002043257ff061318f2eae9633906af00932
“fx67ll;git config --global user.name “fx67ll <2717745014@qq.com>
2026-07-21 21:23:02 +0800

    optimize

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,6 @@ const store = new Vuex.Store({
 	// 存储全局变量
 	state: {
 		isShowbckbtn: false,
-		btnType: 'default',
 	},
 	// 类似computed计算属性，用于从store的state中派生出一些状态，例如过滤并计算，目前来说用不到
 	// getters: {
@@ -17,24 +16,16 @@ const store = new Vuex.Store({
 	// },
 	// 专注于同步修改State，官方推荐大写mutations操作
 	mutations: {
-		// 同步设置bckbtn的状态
+		// 同步设置返回按钮的显示状态
 		SETBTNSTATE(state, value) {
 			state.isShowbckbtn = value;
-		},
-		// 同步设置bckbtn的类型
-		SETBTNTYPE(state, value) {
-			state.btnType = value;
 		},
 	},
 	// 专注于业务代码，可以进行异步操作
 	actions: {
-		// 异步设置bckbtn的状态
+		// 异步设置返回按钮的显示状态
 		setBtnStateAsync(context, params) {
 			context.commit('SETBTNSTATE', params);
-		},
-		// 异步设置bckbtn的类型
-		setBtnTypeAsync(context, params) {
-			context.commit('SETBTNTYPE', params);
 		},
 	}
 });
